14cfdf1852115d60189e6b0b80629722c5c24b74
Félix <felix.potente@alten.es>
2021-06-15 13:04:27 +0200

    Tema 16 Fin video "16. Eliminar elemento creado de forma dinámica" , "17. Agregar juegos favoritos"  , "18. Template Radio, Check y Switches" y "19. Template Validando Radios, Checks y Switches" FINAL

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
 
-interface Persona{
-  nombre:string;
-  favoritos:Favorito[];
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
 }
 
-interface Favorito{
-  id:number;
-  nombre:string;
+interface Favorito {
+  id: number;
+  nombre: string;
 }
 @Component({
   selector: 'app-dinamicos',
@@ -16,20 +16,34 @@ interface Favorito{
   styles: [
   ]
 })
-export class DinamicosComponent  {
+export class DinamicosComponent {
 
-  persona:Persona ={
-    nombre:'Félix',
-    favoritos:[
-      {id:1, nombre:'Metal Gear'},
-      {id:2, nombre:'Death Stranding'}
+  nuevoJuego: string = '';
+  persona: Persona = {
+    nombre: 'Félix',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear' },
+      { id: 2, nombre: 'Death Stranding' }
     ]
   }
- 
 
 
-  guardar(){
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    };
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego='';
+  }
+
+  guardar() {
     console.log("formulario Posteado");
   }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
+  }
+
 
 }
